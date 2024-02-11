723e35ed93f134455f4deb2c207006d9fef33b81
oliverity <silfirya@yandex.ru>
2024-02-11 17:27:07 +0300

    Step 4. Code quality.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,6 +3,6 @@
 // import path from 'path';
 // import { welcome } from path.join(__dirname, '..', 'src', 'cli.js');
 
-import { welcome } from '../src/cli.js';
+import welcome from '../src/cli.js';
 
 welcome();
